a6765ca762ad888752576e5dc15b1a9d8c865000
Adamu Muhammad <sayyid211@gmail.com>
2024-06-25 12:53:57 +0100

    print n prime numbers

---
+++ alert.js
@@ -0,0 +1,6 @@
+alert("I'm JavaScript!");
+let age = prompt('How old are you?', 0);
+alert(`You are ${age} years old!`);
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss ); // true if OK is pressed
